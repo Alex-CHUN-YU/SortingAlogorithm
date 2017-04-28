dcc25933351b0c2623de98f03ec479dc785a90bf
Alex-CHUN-YU <P76064538@mail.ncku.edu.tw>
2017-04-28 14:00:27 +0800

    Sorting

---
+++ src/SortingAlogorithm/SortingAlogorithm.java
@@ -0,0 +1,21 @@
+package SortingAlogorithm;
+
+/**
+ *
+ * MainFunction Test.
+ *
+ * @version 1.0 2017年4月28日
+ * @author Alex-CHUN-YU
+ *
+ */
+   public class SortingAlogorithm {
+
+   /**
+    * @param args system define
+    */
+   public static void main(final String[] args) {
+            // TODO Auto-generated method stub
+
+    }
+
+}
