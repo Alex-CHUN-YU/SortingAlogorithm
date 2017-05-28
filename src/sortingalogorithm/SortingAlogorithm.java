12ec0a371874ba5cf2a6f1af33cbc0230ff11167
Alex-CHUN-YU <P76064538@mail.ncku.edu.tw>
2017-05-28 14:27:36 +0800

    input size = six

--- src/sortingalogorithm/SortingAlogorithm.java
+++ src/sortingalogorithm/SortingAlogorithm.java
@@ -1,5 +1,7 @@
 package sortingalogorithm;
 
+import java.util.Scanner;
+
 /**
  *
  * MainFunction Test.
@@ -14,30 +16,58 @@ public class SortingAlogorithm {
     * @param args system define
     */
    public static void main(final String[] args) {
+	   
+	   //Scanner Your Input
+	   System.out.println("Please Input Number(size = 6)");
+	   @SuppressWarnings("resource")
+	   Scanner scanner =new Scanner(System.in);
+	   String inputList = scanner.nextLine();
+	   String [] strData= inputList.split(" ");
+	   int[] data = new int[6];
+	   int[] data1 = new int[6];
+	   int[] data2 = new int[6];
+	   
+	   //Input Size Error
+	   while (strData.length != 6) {
+		   System.out.println("Please Input Number(size = 6)");
+		   scanner =new Scanner(System.in);
+		   inputList = scanner.nextLine();
+		   strData= inputList.split(" ");
+		   data = new int[6];
+		   data1 = new int[6];
+		   data2 = new int[6];
+	   }
+	   for (int i = 0; i < strData.length; i++) {
+		   	data[i] = Integer.parseInt(strData[i]);
+		   	data1[i] = Integer.parseInt(strData[i]);
+		   	data2[i] = Integer.parseInt(strData[i]);
+	   }
+	   System.out.println();
+	   
        //InsertionSort
-       int[] data = {12, 52, 65, 66, 2, 45, 85, 9, 11};
        data = InsertionSort.getInsertionSort(data);
        System.out.print("InsertionSort: ");
        for (int i = 0; i < data.length; i++) {
            System.out.print(data[i] + " ");
        }
        System.out.println();
+       
        //SelectionSort
-       int[] data1 = {12, 65, 52, 2, 66, 85, 45, 11, 9};
        data1 = SelectionSort.getSelectionSort(data1);
        System.out.print("SelectionSort: ");
        for (int i = 0; i < data1.length; i++) {
            System.out.print(data1[i] + " ");
        }
        System.out.println();
+       
        //QuickSort
-       int[] data2 = {65, 52, 66, 45, 2, 85, 9, 11, 12};
        data2 = QuickSort.getQucikSort(0, data2.length - 1, data2);
        System.out.print("QuickSort:     ");
        for (int i = 0; i < data2.length; i++) {
            System.out.print(data2[i] + " ");
        }
-    }
+       
+   }
 }
 
 
